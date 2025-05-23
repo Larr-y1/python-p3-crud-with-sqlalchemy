c3d571ff1354ae6e76b86ad70dcd222f6128129b
Larry <larryotieno51@gmail.com>
2025-05-23 11:39:40 +0300

    crud operations with sqlAlchemy

--- lib/debug.py
+++ lib/debug.py
@@ -2,6 +2,8 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
 
 from sqlalchemy_sandbox import Base, Student
 
@@ -12,5 +14,38 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+   
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    # print(f"New student ID is {albert_einstein.id}.")
+    # print(f"New student ID is {alan_turing.id}.")
+
+    students = session.query(Student)
+
+    print([student for student in students])
     
     import ipdb; ipdb.set_trace()
